ebb873ff271f57ec86f45e34148c0451b62e8362
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-09-03 20:53:47 +0200

    Make cards draggable

--- src/client.ts
+++ src/client.ts
@@ -23,17 +23,58 @@ function createPlayerHandElements(count: number) {
         let element = document.createElement("div");
         element.classList.add("held-card");
         element.style.setProperty("--index", i.toString());
-        element.onmouseover = mouseCard.bind(null, element);
+        element.onmouseover = mouseHoverCard.bind(null, element);
+        element.onmousedown = cardMouseDown.bind(null, element);
         playerDeck?.append(element);
     }
     playerDeck?.style.setProperty("--count", count.toString());
 }
 
 let zIndex = 0;
-function mouseCard(element: any) {
+function mouseHoverCard(element: any) {
     zIndex++;
     element.style.zIndex = zIndex;
 };
+
+let drag = {
+    mouseX: 0,
+    mouseY: 0,
+    startX: 0,
+    startY: 0,
+    element: null,
+    active: false,
+};
+function cardMouseDown(element: any, event: MouseEvent) {
+    drag.startX = event.clientX;
+    drag.startY = event.clientY;
+    drag.element = element;
+    drag.active = true;
+    element.style.transition = "0s";
+    element.classList.add("dragged-card");
+};
+
+document.addEventListener("mouseup", (event) => {
+    drag.active = false;
+    (<any>drag.element).style.transition = "";
+    (<any>drag.element).classList.remove("dragged-card");
+    (<any>drag.element).style.transform = ``;
+});
+document.addEventListener("mousemove", (event) => {
+    if (!drag.active)
+        return;
+    drag.mouseX = event.clientX;
+    drag.mouseY = event.clientY;
+    let deltaX = drag.mouseX - drag.startX;
+    let deltaY = drag.mouseY - drag.startY;
+    if (drag.element) {
+        deltaX /= 1.4;
+        deltaY /= 1.4;
+        deltaY -= 40;
+        (<any>drag.element).style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+        // (<any>drag.element).style.left = `${deltaX}px`;
+        // (<any>drag.element).style.top = `${deltaY}px`;
+    }
+});
 
 createGridElements(game.board.size);
 createPlayerHandElements(5);
