8f88eb2b4e3bbbbb909749d628fb020b8618b5f2
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-09-05 15:19:15 +0200

    Fix clone on arrays

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,16 @@
 export function clone<Type extends Object>(object: Type): Type {
+    if (object instanceof Array) {
+        let cloned = [];
+        for (let value of object) {
+            if (value instanceof Object) {
+                value = clone(value);
+            }
+            cloned.push(value);
+        }
+        return <any>cloned;
+    }
     let cloned = Object.create(object);
+
     for (let [key, value] of Object.entries(object)) {
         if (value instanceof Object) {
             value = clone(value);
@@ -8,3 +19,5 @@ export function clone<Type extends Object>(object: Type): Type {
     }
     return cloned;
 }
+
+(<any>globalThis).clone = clone;
