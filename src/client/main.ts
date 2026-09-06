12fccb61b7f6f1e4e7efc257db6587bf35dc4301
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-09-06 21:49:52 +0200

    Add functions for encoding and decoding network packets

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,7 +1,7 @@
 import { cardRegistry } from "../cards";
 import { Game, Player } from "../engine";
 import { PlayerPacket, playerPacketsRegistry } from "../player_packets";
-import { ServerPacket } from "../server_packets";
+import { ServerPacket, serverPacketRegistry } from "../server_packets";
 import { clone, decodePacket, EncodedPacket, encodePacket } from "../utils";
 import { Client } from "./client";
 import { loadUi } from "./ui";
@@ -10,9 +10,15 @@ let game = new Game(5);
 function callback(playerPacket: PlayerPacket): ServerPacket {
     // encode and re-decode packets to simulate network transmission
     // for the sake of ensuring parity.
+
     let packetEncoded = encodePacket(playerPacket, playerPacketsRegistry);
     let decodedPacket = decodePacket(packetEncoded, playerPacketsRegistry);
-    return game.processPlayerAction(decodedPacket);
+
+    let receivedPacket = game.processPlayerAction(decodedPacket);
+    let receivedPacketEncoded = encodePacket(receivedPacket, serverPacketRegistry);
+    let receivedPacketDecoded = decodePacket(receivedPacketEncoded, serverPacketRegistry);
+
+    return receivedPacketDecoded;
 
 }
 let client = new Client(clone(game.board), clone(<Player>game.players[0]), callback);
