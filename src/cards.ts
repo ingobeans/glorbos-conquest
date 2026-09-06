dc356651a44951bbaaeb7b96c1b05e79e4e2cbc5
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-09-06 23:18:42 +0200

    Make cards track their index (for serialisation purposes)

--- src/cards.ts
+++ src/cards.ts
@@ -8,10 +8,12 @@ export class Card {
     maxHealth: number = 0;
     health: number = 0;
     entityId: number = -1;
+    cardIndex: number = -1;
     canStack(self: PlacedCard, other: PlacedCard): boolean { return false; }
 }
 
 export let cardRegistry: Card[] = [];
 export function registerCard(card: Card) {
+    card.cardIndex = cardRegistry.length;
     cardRegistry.push(card);
 }
