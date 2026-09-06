c8830214a03c34ee58a62b4bdd808e3d3c9ee8b2
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-09-06 21:14:09 +0200

    Implement network packet

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,11 +1,21 @@
 import { cardRegistry } from "../cards";
 import { Game, Player } from "../engine";
-import { clone } from "../utils";
+import { PlayerPacket, playerPacketsRegistry } from "../player_packets";
+import { ServerPacket } from "../server_packets";
+import { clone, decodePacket, EncodedPacket, encodePacket } from "../utils";
 import { Client } from "./client";
 import { loadUi } from "./ui";
 
 let game = new Game(5);
-let client = new Client(clone(game.board), clone(<Player>game.players[0]), game.processPlayerAction.bind(game));
+function callback(playerPacket: PlayerPacket): ServerPacket {
+    // encode and re-decode packets to simulate network transmission
+    // for the sake of ensuring parity.
+    let packetEncoded = encodePacket(playerPacket, playerPacketsRegistry);
+    let decodedPacket = decodePacket(packetEncoded, playerPacketsRegistry);
+    return game.processPlayerAction(decodedPacket);
+
+}
+let client = new Client(clone(game.board), clone(<Player>game.players[0]), callback);
 
 console.log(cardRegistry);
 
--- src/utils.ts
+++ src/utils.ts
@@ -24,13 +24,35 @@ export function clamp(v: number, min: number, max: number): number {
     return Math.max(Math.min(v, max), min);
 }
 
-export class Packet<Type> {
+export class EncodedPacket<Type> {
     typeIndex: number;
     value: Type;
     constructor(typeIndex: number, value: Type) {
         this.typeIndex = typeIndex;
         this.value = value;
     }
+}
+
+export function encodePacket(data: Object, registry: any[]): string {
+    let index = -1;
+    for (let [i, ty] of registry.entries()) {
+        if (data instanceof ty.constructor) {
+            index = i;
+        }
+    }
+    let packet = new EncodedPacket(index, data);
+    let packetEncoded = JSON.stringify(packet);
+    return packetEncoded;
+}
+
+export function decodePacket<Type>(encoded: string, registry: Type[]): Type {
+    let decodedPacket = <EncodedPacket<Object>>JSON.parse(encoded);
+    let type = registry[decodedPacket.typeIndex];
+    if (!type)
+        throw Error("bad packet");
+
+    let extractedPacket = Object.setPrototypeOf(decodedPacket.value, type);
+    return extractedPacket;
 }
 
 (<any>globalThis).clone = clone;
